645fbeb9c72a3d4ca79b0333e789625df4069712
silver_volt4 <gdpancake5@gmail.com>
2025-08-15 18:52:19 +0200

    feat: more precise CLRSLT1 parsing

--- src/blocks/colslt1.rs
+++ src/blocks/colslt1.rs
@@ -17,12 +17,36 @@ pub struct Colors {
 #[derive(Debug, Serialize)]
 #[cfg_attr(feature = "tsify", derive(Tsify))]
 pub struct Color {
+    pub primary: RGBA,
+    pub extra1: RGBA,
+    pub extra2: RGBA,
+    pub extra3: RGBA,
+    pub id: u32,
+    pub name: String,
+}
+
+#[derive(Debug, Serialize)]
+#[cfg_attr(feature = "tsify", derive(Tsify))]
+pub struct RGBA {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
-    pub id: u32,
-    pub name: String,
+}
+
+impl RGBA {
+    fn full_rgb_value(nibble: u8) -> u8 {
+        nibble << 4 | nibble
+    }
+
+    pub fn from_bytes(bytes: [u8; 2]) -> Self {
+        RGBA {
+            r: Self::full_rgb_value(bytes[1].pick_bits(4..=7)),
+            g: Self::full_rgb_value(bytes[1].pick_bits(0..=3)),
+            b: Self::full_rgb_value(bytes[0].pick_bits(4..=7)),
+            a: Self::full_rgb_value(bytes[0].pick_bits(0..=3)),
+        }
+    }
 }
 
 impl Colors {
@@ -31,17 +55,13 @@ impl Colors {
     }
 }
 
-fn full_rgb_value(nibble: u8) -> u8 {
-    nibble << 4 | nibble
-}
-
 impl Color {
     pub fn from_bytes(bytes: [u8; 0x4c]) -> Color {
         Color {
-            r: full_rgb_value(bytes[5].pick_bits(4..=7)),
-            g: full_rgb_value(bytes[5].pick_bits(0..=3)),
-            b: full_rgb_value(bytes[4].pick_bits(4..=7)),
-            a: full_rgb_value(bytes[4].pick_bits(0..=3)),
+            primary: RGBA::from_bytes(bytes[4..=5].try_into().unwrap()),
+            extra1: RGBA::from_bytes(bytes[6..=7].try_into().unwrap()),
+            extra2: RGBA::from_bytes(bytes[8..=9].try_into().unwrap()),
+            extra3: RGBA::from_bytes(bytes[10..=11].try_into().unwrap()),
             id: u32::from_le_bytes(bytes[0..=3].try_into().unwrap()),
             name: read_utf16_name::<0x40>(bytes[0xc..=0x4b].try_into().unwrap()),
         }
