efae14a85c3b369214f857db2802c758c66f5572
silver_volt4 <gdpancake5@gmail.com>
2025-08-15 19:38:21 +0200

    feat: add functions for lzss compression

--- src/lzss.rs
+++ src/lzss.rs
@@ -1,6 +1,22 @@
 use std::io::{Cursor, Read, Seek};
 
+pub use nintendo_lz::CompressionLevel;
+
 use crate::error::GenericResult;
+
+pub fn compress_from_slice(slice: &[u8], level: CompressionLevel) -> GenericResult<Vec<u8>> {
+    let mut out: Vec<u8> = vec![];
+    nintendo_lz::compress(&slice, &mut out, level)?;
+    Ok(out)
+}
+
+pub fn compress_lz10_from_slice(slice: &[u8]) -> GenericResult<Vec<u8>> {
+    compress_from_slice(slice, CompressionLevel::LZ10)
+}
+
+pub fn compress_lz11_from_slice(slice: &[u8], max_repeat_size: u32) -> GenericResult<Vec<u8>> {
+    compress_from_slice(slice, CompressionLevel::LZ11(max_repeat_size))
+}
 
 pub fn decompress_from_reader<R: Read + Seek>(reader: &mut R) -> GenericResult<Vec<u8>> {
     nintendo_lz::decompress(reader)
