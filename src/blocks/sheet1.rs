35ee2722c1ab2528e1df10f0ccb02e89a9c837a9
silver_volt4 <gdpancake5@gmail.com>
2025-08-16 14:27:42 +0200

    feat: add sticker support
    This includes Miis, App icons and Badge arcade badges.
    I have not yet tested how the game reacts to certain values, so the struct model may not be 100% perfect.

--- src/blocks/sheet1.rs
+++ src/blocks/sheet1.rs
@@ -8,18 +8,63 @@ use crate::{bits::PickBit, read::ReadExt};
 #[derive(Debug, Serialize)]
 #[cfg_attr(feature = "tsify", derive(Tsify), tsify(into_wasm_abi))]
 pub struct Sheet {
-    pub strokes: Vec<SheetStroke>,
+    pub events: Vec<SheetEvent>,
+    pub secret_page: bool,
+}
+
+#[derive(Debug, Serialize)]
+#[cfg_attr(feature = "tsify", derive(Tsify), tsify(namespace, into_wasm_abi))]
+pub enum SheetEventData {
+    PaintEvent {
+        continue_to_next: bool,
+        thick_pen: bool,
+        color_index: u8,
+    },
+    StickerEvent {
+        sticker_index: u8,
+        rotation: u8,
+        is_mii: bool,
+        is_badge: bool,
+        facial_expression: u8, // should be 0 if is_mii == false
+    },
+    PhotoEvent,
+    Unknown {
+        event_type: u8,
+    },
+}
+
+impl SheetEventData {
+    fn from_bytes(bytes: [u8; 4]) -> Self {
+        if !bytes[0].pick_bit(3) {
+            return Self::PaintEvent {
+                continue_to_next: bytes[2].pick_bit(6),
+                color_index: bytes[3].pick_bits(0..=2),
+                thick_pen: bytes[3].pick_bit(3),
+            };
+        } else if bytes[0].pick_bit(2) {
+            return Self::StickerEvent {
+                sticker_index: bytes[2].pick_bits(6..=7),
+                rotation: bytes[3].pick_bits(4..=7),
+                is_mii: bytes[0].pick_bit(1),
+                is_badge: bytes[0].pick_bit(3),
+                facial_expression: bytes[3].pick_bits(0..=3),
+            };
+        } else if bytes[0].pick_bit(3) {
+            return Self::PhotoEvent;
+        }
+        return Self::Unknown {
+            event_type: !bytes[0].pick_bits(0..=3),
+        };
+    }
 }
 
 #[derive(Debug, Serialize)]
 #[cfg_attr(feature = "tsify", derive(Tsify))]
-pub struct SheetStroke {
+pub struct SheetEvent {
     pub x: u8,
     pub y: u8,
-    pub draw_line: bool,
-    pub style_color: u8,
     pub style_3d: bool,
-    pub style_bold: bool,
+    pub data: SheetEventData,
 }
 
 impl Sheet {
@@ -28,15 +73,13 @@ impl Sheet {
     }
 }
 
-impl SheetStroke {
-    pub fn from_bytes(bytes: [u8; 4]) -> SheetStroke {
-        SheetStroke {
-            x: (bytes[2] & 0x0F) << 4 | bytes[1].pick_bits(4..=7),
-            y: (bytes[1] & 0x0F) << 4 | bytes[0].pick_bits(4..=7),
-            draw_line: bytes[2].pick_bit(6),
-            style_color: bytes[3].pick_bits(0..=2),
+impl SheetEvent {
+    pub fn from_bytes(bytes: [u8; 4]) -> SheetEvent {
+        SheetEvent {
+            x: (bytes[2].pick_bits(0..=3)) << 4 | bytes[1].pick_bits(4..=7),
+            y: (bytes[1].pick_bits(0..=3)) << 4 | bytes[0].pick_bits(4..=7),
             style_3d: bytes[2].pick_bit(5),
-            style_bold: bytes[3].pick_bit(3),
+            data: SheetEventData::from_bytes(bytes),
         }
     }
 }
@@ -46,16 +89,20 @@ impl TryFrom<&[u8]> for Sheet {
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let mut reader = Cursor::new(value);
-        reader.read_u32_le()?;
+        reader.read_u32_le()?; // seems to be constant
         let num_blocks = reader.read_u32_le()?;
-        reader.set_position(0x40);
 
-        let mut sheet: Sheet = Sheet { strokes: vec![] };
+        let mut sheet: Sheet = Sheet {
+            events: vec![],
+            secret_page: reader.read_const_num_of_bytes::<1>()?[0] != 0,
+        };
+
+        reader.set_position(0x40);
 
         for _ in 0..num_blocks {
             sheet
-                .strokes
-                .push(SheetStroke::from_bytes(reader.read_const_num_of_bytes()?));
+                .events
+                .push(SheetEvent::from_bytes(reader.read_const_num_of_bytes()?));
         }
 
         Ok(sheet)
